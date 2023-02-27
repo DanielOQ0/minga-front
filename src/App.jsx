49b387a2f8934650a4381b4c8a95d76a7b18c8e2
DanielOQ <danielorozco0208@gmail.com>
2023-02-27 12:33:42 -0500

    m-04

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
 import "./App.css";
-import Footer from "./components/Footer/Footer.jsx";
 import Index from "./components/Index/Index.jsx";
 import IndexLayout from "./layouts/IndexLayout/IndexLayout.jsx";
 
 function App() {
   return (
     <div className="App">
-      <IndexLayout>
+       <IndexLayout>
         <Index/>
       </IndexLayout>
-    </div>
+    </div> 
   );
 }
 
